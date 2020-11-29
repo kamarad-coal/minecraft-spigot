634bc9d56e945a31040b92647861c7a78ea3ec0f
Alex Renoki <rennokki@gmail.com>
2020-11-29 09:47:17 +0200

    Pre-check session files and delete them if needed.

--- minecraft/run.sh
+++ minecraft/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [ ! -f /minecraft/world/session.lock ]; then
+    rm -rf /minecraft/world/session.lock
+fi
+
+if [ ! -f /minecraft/world_nether/session.lock ]; then
+    rm -rf /minecraft/world_nether/session.lock
+fi
+
+if [ ! -f /minecraft/world_the_end/session.lock ]; then
+    rm -rf /minecraft/world_the_end/session.lock
+fi
+
 java $JAR_ARGS -jar spigot.jar
